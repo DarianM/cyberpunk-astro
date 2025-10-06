55585883f47fd7468e71cdd1b499ef3d0f015552
darian <darian410@gmail.com>
2025-10-06 13:44:53 +0300

    add production link back

---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+import type { APIContext, MiddlewareNext } from "astro";
+
+export async function onRequest(context: APIContext, next: MiddlewareNext) {
+  const response = await next();
+
+  // Set Cross-Origin-Opener-Policy header
+  // response.headers.set("Cross-Origin-Opener-Policy", "same-origin");
+
+  return response;
+}
