d3507ecc9d350d2c09051ff4434963b631c539a9
darian <darian410@gmail.com>
2025-10-06 13:52:09 +0300

    production #2

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ export async function onRequest(context: APIContext, next: MiddlewareNext) {
   const response = await next();
 
   // Set Cross-Origin-Opener-Policy header
-  // response.headers.set("Cross-Origin-Opener-Policy", "same-origin");
+  response.headers.set("Cross-Origin-Opener-Policy", "same-origin");
 
   return response;
 }
