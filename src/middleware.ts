e9b6e5894136042273b501b90b5b0c9385895ed1
Darian <darian410@gmail.com>
2025-10-28 00:48:35 +0200

    cleanup some

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,8 @@ import type { APIContext, MiddlewareNext } from "astro";
 export async function onRequest(context: APIContext, next: MiddlewareNext) {
   const response = await next();
 
+  response.headers.set("Access-Control-Allow-Origin", "*");
+
   // Set Cross-Origin-Opener-Policy header
   response.headers.set("Cross-Origin-Opener-Policy", "same-origin");
 
