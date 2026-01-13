7017a49b7d5637e139be10dd9ea4de0fb7af2340
Darian <darian410@gmail.com>
2026-01-13 23:44:28 +0200

    add csp

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,5 +8,11 @@ export async function onRequest(context: APIContext, next: MiddlewareNext) {
   // Set Cross-Origin-Opener-Policy header
   response.headers.set("Cross-Origin-Opener-Policy", "same-origin");
 
+  // response.headers.set('Content-Security-Policy',
+  //   "script-src 'self' 'unsafe-inline' http://127.0.0.1:5173 http://localhost:5173; " +
+  //   "img-src 'self' data: http://127.0.0.1:5173 http://localhost:5173; " +
+  //   "connect-src 'self' http://127.0.0.1:* http://localhost:*"
+  // );
+
   return response;
 }
