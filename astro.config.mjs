59f93f37ffa3b6109204284e23cc2f4dc351b14d
Darian <darian410@gmail.com>
2026-04-21 13:34:04 +0300

    nit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: 'https://darian.xyz',
-  output: 'static'
+  site: "https://darianm.xyz",
+  output: "static",
 });
